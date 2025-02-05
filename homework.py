08587c2e82a5f1b093ed0051b08659cc8c6472c4
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-05 18:24:34 +0400

    homework

---
+++ homework.py
@@ -0,0 +1,3 @@
+congrats="ronaldo, i want to congratulate YOU FOR reaching nine hundred career goals. I wish you a sucesful and neverending career. sincerely, Beshoy"
+uppercase_congrats=congrats.lower()
+print(uppercase_congrats)
