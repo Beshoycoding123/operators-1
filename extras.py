9b9e93fdc5e76fb7f6a75bd2110a73d7fa40a7e8
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-05 19:33:42 +0400

    project1

---
+++ extras.py
@@ -0,0 +1,3 @@
+print("please answer the following questions")
+name=input("enter your email: ")
+password=input("enter your password: ")
--- homework2.py
+++ homework2.py
@@ -2,3 +2,5 @@ print("Hello, welcome to my website. it is for finding the square root of the nu
 number=int(input("Input your number here: "))
 sqrt_value=int(number**0.5)
 print("the square root of your input is",sqrt_value)
+b=float(input("input your age: "))
+print(int(b))
