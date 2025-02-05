e951b8702e939a5a9b94a0113b31cdcf5321cae0
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-05 19:01:07 +0400

    homework

--- homework.py
+++ homework.py
@@ -1,3 +1,3 @@
-congrats="ronaldo, i want to congratulate YOU FOR reaching nine hundred career goals. I wish you a sucesful and neverending career. sincerely, Beshoy"
+congrats="ronaldo, i want to congratulate you for reaching nine hundred career goals. I wish you a sucesful and neverending career. sincerely, Beshoy"
 uppercase_congrats=congrats.lower()
 print(uppercase_congrats)
---
+++ homework2.py
@@ -0,0 +1,4 @@
+print("Hello, welcome to my website. it is for finding the square root of the number you entered") # type: ignore
+number=int(input("Input your number here: "))
+sqrt_value=int(number**0.5)
+print("the square root of your input is",sqrt_value)
